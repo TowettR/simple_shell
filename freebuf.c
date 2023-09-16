13963fe97163dd4d842425f4772938972c89b8c0
TowettR <reins5595@gmail.com>
2023-09-16 15:13:12 -0700

    simple_shell part 2

---
+++ freebuf.c
@@ -0,0 +1,16 @@
+/**
+ * freebuf - Frees a pointer and sets it to NULL
+ * @ptr: A pointer to be freed and set to NULL
+ *
+ * Return: 1 if the pointer was freed, 0 if not
+ */
+int freebuf(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
